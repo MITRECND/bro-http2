f89193c784d92b03d70c9493090ee48589bb101d
Murad Khan <mkhan@mitre.org>
2018-04-16 09:58:09 -0400

    Version bump

--- src/Plugin.cc
+++ src/Plugin.cc
@@ -14,7 +14,7 @@ public:
         plugin::Configuration config;
         config.name = "http2::HTTP2";
         config.version.major = 0;
-        config.version.minor = 1;
+        config.version.minor = 2;
         config.description = "Hypertext Transfer Protocol Version 2 analyzer";
         return config;
         }
